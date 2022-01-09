f822fa43c72da79e7262e5fa381f32ccb905b895
MoyoungY <moyoungyuan@foxmail.com>
2022-01-09 15:45:07 +0800

    ep07: Add premake && update .gitignore

---
+++ premake5.lua
@@ -0,0 +1,79 @@
+workspace "MOY"
+	configurations {"Debug", "Release", "Dist"}
+	architecture "x64"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MOY"
+	location "MOY"
+	kind "SharedLib"
+	language "C++"
+	files{"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	includedirs{"%{prj.name}/ext/spdlog/include"}
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}" )
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MOY_PLATFORM_WINDOWS",
+			"MOY_DLL_BUILD"
+		}
+		
+		postbuildcommands 
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir.. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "MOY_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "MOY_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MOY_Dist"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	files{"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	includedirs{"MOY/ext/spdlog/include", "MOY/src/"}
+
+	links{ "MOY" }
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}" )
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MOY_PLATFORM_WINDOWS",
+			"MOY_APP_BUILD"
+		}
+	
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "HZ_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_Dist"
+		optimize "On"
